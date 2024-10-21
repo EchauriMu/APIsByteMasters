ffaa4473e1e1fe6273596e888afb1eb189b44c6b
CarloYoli06 <casaamba@gmail.com>
2024-10-21 08:49:55 -0700

    GestionProductosPrecios

--- src/api/v1/routes/index.js
+++ src/api/v1/routes/index.js
@@ -1,14 +1,19 @@
 //Commerce
 import { Router } from 'express';
 import config from '../../../config/config';
+
 // Import Routes
 import preciosRoutes from './precios.routes';
+import prodPreciosRoutes from './prodPrecios.routes.js';
+
 const routerAPI = (app) => {
   const router = Router();
   const api = config.API_URL;
   app.use(api, router);
   // Routes
-  router.use('/listas-precios', preciosRoutes);
+  //router.use('/listas-precios', preciosRoutes);
+
+  router.use('/listas-precios', prodPreciosRoutes);
   return router;
 };
 module.exports = routerAPI;
